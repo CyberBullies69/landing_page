c8e97217bd7f50473667961c870856fb030ca34d
mihirbhadak <98308594+mihirbhadak@users.noreply.github.com>
2024-04-15 16:17:11 +0530

    modified:   app/layout.tsx 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      
+      <div className="bg-gradient-to-b from-white to-indigo-400 h-screen">hi </div>
     </>
   );
 }
