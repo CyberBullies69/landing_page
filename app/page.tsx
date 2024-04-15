b636c92ddada43cad3d7f0fef59a12bb5d050bdc
mihirbhadak <98308594+mihirbhadak@users.noreply.github.com>
2024-04-15 16:33:35 +0530

    new file:   components/Section1.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Section1 from "@/components/Section1";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-gradient-to-b from-white to-indigo-400 h-screen">hi</div>
+      <div className="bg-gradient-to-b from-white to-indigo-400 h-screen">
+        <Section1/>
+      </div>
     </>
   );
 }
---
+++ components/Section1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <>
+        <h1>DockLink: Revolutionizing Healthcare Access for</h1>
+        <h1>Professional and Employers</h1>
+    </>
+  )
+}
+
+export default Section1
