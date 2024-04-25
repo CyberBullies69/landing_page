5f574c62491c7f94b91b2f4bbe029a3eb4764709
hEeNaaaaa <144811051+hEeNaaaaa@users.noreply.github.com>
2024-04-25 11:13:23 +0530

    modified:   components/Section1.tsx

--- components/Section1.tsx
+++ components/Section1.tsx
@@ -6,7 +6,7 @@ const Section1 = () => {
   return (
     <>
       <div className='flex flex-col justify-center items-center mt-20'>
-        <h1 className='text-black font-bold text-5xl'>DockLink: Revolutionizing Healthcare Access for</h1>
+        <h1 className='text-black font-bold text-5xl'>DockLink: Revolutionizing </h1>
         <h1 className='text-blue-600 font-bold text-5xl'>Professional and Employers</h1>
         <p className='max-w-screen-lg mt-2 text-center'>At DocLink, we believe in revolutionizing the way healthcare is accessed and delivered. Our comprehensive platform caters to the needs of medical professionals and employers, providing a seamless and innovative solution for all stakeholders in the healthcare industry.</p>
         <Button className='mt-12 px-10'>Get Started</Button>
