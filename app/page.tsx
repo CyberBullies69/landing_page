1cc390ba63b72522393c44b8764af6b8edcb9373
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-04-29 11:15:17 +0530

    modified:   app/page.tsx 	modified:   components/text-generate-effect.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import Section1 from "@/components/Section1";
 import Section2 from "@/components/Section2";
-import Section3 from "@/components/Section3";
-import Image from "next/image";
 
 export default function Home() {
   return (
